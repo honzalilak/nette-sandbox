b2169d7cbfa1186015cd52c6bb0d25c8a16fffbb
Václav Makeš <info@makes.cz>
2012-12-31 11:24:29 +0100

    convert modelLoader to DI

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -5,17 +5,4 @@
  */
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
-
-    /**
-     * shortener to be used in presenters
-     * makes model accessible via $this->getModel('foo');
-     * 
-     * @param string $model
-     * @return 
-     */
-    public function getModel($model) {
-		return $this->context->modelLoader->loadModel($model);
-    }
-    
-
 }
